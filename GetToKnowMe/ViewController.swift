a12407865af7de48a7044cfce02c9bd82c0cfca5
Thomas Koutsidis <thomaskoutsidis@thomass-macbook-pro.local>
2018-06-26 15:56:19 -0400

    Initial Commit

---
+++ GetToKnowMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetToKnowMe
+//
+//  Created by Thomas Koutsidis on 6/25/18.
+//  Copyright © 2018 Thomas Koutsidis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
